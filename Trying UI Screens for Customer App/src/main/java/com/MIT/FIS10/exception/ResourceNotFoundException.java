7b006f705647552b21b44d48b9805d75a4d1cb9d
Arkadeep Ganguly <arkadeepganguly14@gmail.com>
2025-04-18 12:01:27 +0530

    Add files via upload

---
+++ Trying UI Screens for Customer App/src/main/java/com/MIT/FIS10/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package com.MIT.FIS10.exception; 
+ 
+public class ResourceNotFoundException extends RuntimeException { 
+ 
+    /** 
+  *  
+  */ 
+ private static final long serialVersionUID = 1L; 
+ 
+ public ResourceNotFoundException(String message) { 
+        super(message); 
+
+    } 
+ 
+    public ResourceNotFoundException(String message, Throwable cause) { 
+        super(message, cause); 
+    } 
+} 
